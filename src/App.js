6de03038daeb4af031060f8113c2483962aace68
Nick Carducci <nmcarducci@gmail.com>
2022-02-14 01:51:55 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1241,6 +1241,9 @@ export default class App extends React.Component {
                 : "17px"
           }}
         >
+          Pamolina, when you wen to Iceland at 21 was it with your mom’s “money?”
+          <br/>
+          <br/>
           Bill O'Reilly: "Every dollar you’ve 'earned' of $170t good will and
           collateralized 'value,' $2t checking, Savers’ donee beneficiary
           land-exploration rights (20% of U.S.). Is competitiveness how much you
